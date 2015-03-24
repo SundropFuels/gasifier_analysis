ec9044d5afd7ac1e4bb0fcf9ca79394e2f83669a
ryontracy <ryon.tracy@sundropfuels.com>
2015-03-24 10:43:11 -0600

    Added comments to gasifier_analysis.py

--- gasifier_analysis.py
+++ gasifier_analysis.py
@@ -14,13 +14,15 @@ class GasifierDataAnalysis:
     """The basic data analysis class for gasifier experiments"""
 
     def __init__(self, run_id, user, password, run_information = None):
-        #Create the gasifier data frame, and load the data from the SQL database (this will be automated through the interface later)
-               
+        #Create the gasifier data frame, and load the data from the SQL database 
+        
+	# Create connection for obtaining raw time series data from lab_run_db       
         self.interface_raw = db.db_interface(host = "192.168.13.51", user = user, passwd = password)
         self.interface_raw.connect()
         q = db.use_Query("lab_run_db")
         self.interface_raw.query(q)
 
+	# Create connection for obtaining run information and uploading results to lab_proc_db
         self.interface_proc = db.db_interface(host = "192.168.13.51", user = user, passwd = password)
         self.interface_proc.connect()
         q = db.use_Query("lab_proc_db")
@@ -28,6 +30,7 @@ class GasifierDataAnalysis:
 
         self.run_id = run_id
 
+	#Create run information container object to house experiment metadata such as start and start times, carbon in biomass, moisture in biomass, etc...
         self.run_info = RunInformation()
         #self.run_info.info = run_information
         self._load_run_info()
@@ -38,7 +41,7 @@ class GasifierDataAnalysis:
 
         #see if we can get a complete view from this
         self.run_info.SQL_load(self.interface_proc, table = 'gas_run_info_tbl', run_id = self.run_id)
-        #set up the tube size
+        #set up the tube size based on tube inner diameter from database and known length of 24 inches.  Volume is stored in in^3 and converted by code to m^3 as appropriate later on
         if self.run_info.info['tube_dia'] == 2.0:
             self.reactor_size = (24.0*1.5*1.5*np.pi/4, 'in^3')
             self.tube_id = [1.5, 'in']
@@ -51,15 +54,17 @@ class GasifierDataAnalysis:
         else:
             self.reactor_size = (None, None)
         
-        #WILL NEED TO DO THE APPROPRIATE GLOSSARY SWITCHES OR JUST RENAME THINGS IN RUN_INFO TO THE RIGHT STUFF
 
     def _load_timeseries_data(self):
         """Loads the timeseries data into the database"""
         
+	#Creates object self.gts for gasifier time series
         self.gts = GasifierProcTS(start = self.run_info.info['ss_start'], end = self.run_info.info['ss_stop'])
-        self.gts.SQL_load(self.interface_raw,'gasifier_lv_GC_view') #This line needs to automatically load the units
-        #Need to build the glossary using the SQL tools
+        self.gts.SQL_load(self.interface_raw,'gasifier_lv_GC_view') 
+
+	#Set up query for obtaining glossary and units data.  Change table as appropriate for new systems.
         q = db.select_Query(objects = ['tag_number', 'simple_name', 'units'], table = "tag_glossary_tbl")
+	#Build glossary as a dictionary from glossary table.
 	glossary = self.interface_raw.query(q)
 	self.glossary = {}
 	self.gl_units = {}
@@ -67,20 +72,26 @@ class GasifierDataAnalysis:
             self.glossary[row['tag_number']] = row['simple_name']
             self.gl_units[row['simple_name']] = row['units']
         
+	#Change column names to 'simple name's from glossary to make calculations more human readable
         self.gts.glossary_replace(self.glossary)
         
+	#Set units from glossary information
         self.gts.set_units(self.gl_units)
+	#Replaces None's with nan's to prevent limitations with numpy calculations
         self.gts.replace_None_w_nan_all()
         
     def _setup_standard_streams(self):
         """Sets up the standard streams and material compositions for analysis"""
+
+	# Define biomass feed stream.  Moisture info is from run information table and stored in run_info object.
         biomass_feed = Stream('biomass_feed',flowrate = self.gts.val_units('mass_flow_brush_feeder'),composition = {'H2O':self.run_info['moisture']/100.0, 'biomass':1.00-self.run_info['moisture']/100.0}, basis = "mass")
-        #Need to add back the enthalpy of vaporization to the biomass phase here -- this is due to two phase considerations
+        #Need to add back the enthalpy of vaporization to the biomass phase here -- this is due to two phase considerations.  This is so the enthalpy of this inlet stream can be estimated for enthalpy change calculations.
         conv = uc.UnitConverter()
         #I just created the biomass feed stream, so the reserve will be in 'W'
         biomass_feed.enthalpy_reserve[0] += (conv.convert_units(biomass_feed.flowrate[0], biomass_feed.flowrate[1], 'kg/s')*biomass_feed.composition['H2O']/0.018*-44010.0) #Heat of vaporization of water
         #NOTE -- this will ONLY WORK if the temperatulre is AT OR NEAR 25 C -- otherwise, we need a specific heat correction as well; we only have an Hf correction
         
+	#Establish gas types based on information in run_info_tbl
         if self.gts['entrainment_gas_type'][0] == 0:
             e_type = "N2"
         elif self.gts['entrainment_gas_type'][0] == 1:
@@ -98,7 +109,9 @@ class GasifierDataAnalysis:
         elif self.gts['makeup_gas_type'][0] == 1:
             m_type = "CO2"
 
+	#Set up stream information from inlet mass flow controllers
         cross_brush_feed = Stream('cross_brush_feed', flowrate = self.gts.val_units('mass_flow_entrainment'), composition = {e_type:1.0}, basis = "std_gas_volume")
+	#Filter out data which would give erroneous negative or low flows when the flow is in fact 0.  See reformer_analysis.py for a more elegant way to accomplish this.  This way looks at intended setpoint in run information and sets to zero if it was supposed to be zero.  It will not catch an instance when the operator mis-sets the setpoint to something other than zero.
         if self.run_info.info['downbed_flow_rate'] < 0.5:
             down_bed_feed = Stream('down_bed_feed', flowrate = (self.gts['mass_flow_down_brush']*0.0,'L/min'), composition = {db_type:1.0}, basis = "std_gas_volume")
         else:
@@ -121,9 +134,11 @@ class GasifierDataAnalysis:
         steam_feed.temperature = (self.gts['temp_steam_reactor_entry'], self.gts.units['temp_steam_reactor_entry'])
         steam_feed.pressure = (self.gts['pressure_reactor_gas_inlet'], self.gts.units['pressure_reactor_gas_inlet'])        
         
+	#Define standard temperatures and pressures used by mass flow controllers during calibration so that actual volumes and molar flow rates can be calculated by the code
         MFC_SP = (101325.0, 'Pa')
         MFC_ST = (70.0, 'F')
 
+	#Define temperatures of inlet gases. This can be done this way or by defining the temperature are an argument when initializing the stream object.
         biomass_feed.set_temperature((25.0, 'C'))
         cross_brush_feed.set_temperature((25.0, 'C'))
         down_bed_feed.set_temperature((25.0, 'C'))
@@ -134,6 +149,7 @@ class GasifierDataAnalysis:
         
         steam_feed.set_temperature(self.gts.val_units('temp_steam_reactor_entry'))
         
+	#Set pressures same way as temperatures above
         biomass_feed.set_pressure(self.gts.val_units('pressure_reactor_gas_inlet'))
         cross_brush_feed.set_pressure(self.gts.val_units('pressure_reactor_gas_inlet'))
         down_bed_feed.set_pressure(self.gts.val_units('pressure_reactor_gas_inlet'))
@@ -156,12 +172,13 @@ class GasifierDataAnalysis:
         argon_tracer_feed.std_pressure = MFC_SP
         methane_gas_feed.std_pressure = MFC_SP
 
-        #Set up exit gas flowrates
+        #Set up exit gas flowrates.  This stream's flowrate is calculated using the argon tracer from the mass spec data in the time series.
            
         gas_exit = self.gts.outlet_stream_from_tracer([argon_tracer_feed],"Molar", "Ar", self.gts['Ar_MS']/100.0, 'gas_exit')
         #Need to set up the exit gas composition now -- we can actually build a variety of different streams here and use them as necessary 
         mass_spec_list = ['C2H2', 'Ar', 'C6H6', 'CO2', 'C2H6', 'C2H4', 'H2S', 'H2', 'CH4', 'C10H8', 'N2', 'C3H8', 'C3H6', 'C7H8', 'H2O', 'CO']
         
+	#Establish a composition for the outlet stream from the time series data.  Also set pressure and temperature.
         composition = {}
         for specie in mass_spec_list:
             composition[specie] = self.gts['%s_MS' % specie]/100.0
@@ -175,7 +192,7 @@ class GasifierDataAnalysis:
         self.gts.units['exit_gas_flowrate'] = 'mol/s'
                
 
-        #interpolate the Ar column to get measurements for GC comparisons
+        #interpolate the Ar column to get measurements for GC comparisons.  This is an unnecessary step.
         GC_list = ['H2', 'CO', 'CO2', 'CH4', 'Ar', 'N2']
         for specie in GC_list:
             
@@ -188,14 +205,17 @@ class GasifierDataAnalysis:
         for specie in GC_list:
             gc_composition[specie] = self.gts["%s_GC_interp" % specie]
             gas_exit_GC.set_composition(gc_composition)
+	#End interpolating GC data.
 
-
+	#Define which streams are inlet streams
         self.gts.inlet_streams = [down_bed_feed, cross_brush_feed, makeup_feed, argon_tracer_feed, methane_gas_feed, steam_feed, biomass_feed]
-        
+        #Define which streams are outlet streams
         self.gts.outlet_streams = [gas_exit]
         
+	#Define which elements to calculate flow rates for at inlet and outlet
         self.gts.proc_elements = ['C', 'H', 'O']
         self.gts.proc_species = ['H2', 'CO', 'CO2', 'CH4', 'C2H6', 'N2', 'C2H4', 'C2H2', 'C6H6', 'Ar', 'C7H8', 'C10H8', 'C3H8', 'C3H6','H2O']
+	#Define inert species to account for when calculating normalized compositionsl
         self.gts.inert_species = ['N2', 'Ar']
         
         #set up the biomass information from the run table
@@ -288,7 +308,7 @@ class GasifierDataAnalysis:
         self.upload_integral_data()
 
     def upload_integral_data(self):
-        #need to build the query elements
+        #Inputting experiment metadata not associated with time series or calculations.  All these columns must exist in the integral data table
         objects = {}
         objects['run_id'] = str(self.run_id)
         objects['moisture'] = str(self.run_info.info['moisture'])
@@ -320,7 +340,7 @@ class GasifierDataAnalysis:
                 print "Crashed trying to upload to integral data table"
 
     def get_analysis_config_list(self):
-        #q = db.use_Query("lab_proc_db")
+        #This uses the analysis configuration table in order to establish which measures it should calculate averages and standard deviations for to upload to the integral table.  In my opinion, it is an unecessary step and can be eliminated.  If this step is included and there is an issue where uploading fails, make sure that the column name exists in the analysis_config_tbl, it's 'active' value is 1, and the column exists in the proc data table and the integral table.
         objects = ['avg_std_cols']
         q = db.select_Query(objects, table = 'analysis_config_tbl', condition_list = ["active = 1"])
         col_list = []
@@ -331,10 +351,11 @@ class GasifierDataAnalysis:
         return col_list
 
     def generate_standard_report(self, rpt_fmt):
-        #generate a standard report -- maybe given an xml format file to direct what goes into the report
+        #This step is its own file gasifier_report.py.  it uses a Latex template and can automatically generate a report showing desired values as well as control charts for important variables for the time series data.  If this is a desired function in the future, look at the gasifier_report.py file and its Latex template.  It is extremely convoluted in its current state.
         pass
 
 def parse_list(txt):
+	#Parses list of experiments to analyze from run list given at run time
     main_list = txt.split(",")
     run_id_list = []
     for sublist in main_list:
